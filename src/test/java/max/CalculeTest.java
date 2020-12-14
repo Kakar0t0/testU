cf686f747275ebe8fc46f9e1cd5698f297156dcd
Maxime Martinez <maxime.martinez.pro@gmail.com>
2020-12-14 14:54:18 +0100

    testU

---
+++ src/test/java/max/CalculeTest.java
@@ -0,0 +1,14 @@
+package max;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
